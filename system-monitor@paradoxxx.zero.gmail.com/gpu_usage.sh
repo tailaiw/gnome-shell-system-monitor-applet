fc534f75eb26da503bcebe89d7256737c523e16e
tailaiw <29800495+tailaiw@users.noreply.github.com>
2020-09-18 20:43:41 -0500

    support multi gpu

--- system-monitor@paradoxxx.zero.gmail.com/gpu_usage.sh
+++ system-monitor@paradoxxx.zero.gmail.com/gpu_usage.sh
@@ -29,7 +29,24 @@ checkcommand()
 # This will print three lines. The first one is the the total vRAM available,
 # the second one is the used vRAM and the third on is the GPU usage in %.
 if checkcommand nvidia-smi; then
-	nvidia-smi -i 0 --query-gpu=memory.total,memory.used,utilization.gpu --format=csv,noheader,nounits | sed 's%, %\n%g'
+	# nvidia-smi -i 0 --query-gpu=memory.total,memory.used,utilization.gpu --format=csv,noheader,nounits | sed 's%, %\n%g'
+	NUM_GPU=$(nvidia-smi -L | wc -l)
+	MEMORY_TOTAL=0
+	MEMORY_USED=0
+	UTILIZATION_GPU=0
+	for ((i=1; i<=NUM_GPU; i++))
+	do
+		MEMORY_TOTAL_THIS_GPU=$(nvidia-smi -i $(($i-1)) --query-gpu=memory.total --format=csv,noheader,nounits)
+		MEMORY_USED_THIS_GPU=$(nvidia-smi -i $(($i-1)) --query-gpu=memory.used --format=csv,noheader,nounits)
+		UTILIZATION_GPU_THIS_GPU=$(nvidia-smi -i $(($i-1)) --query-gpu=utilization.gpu --format=csv,noheader,nounits)
+		MEMORY_TOTAL=$(($MEMORY_TOTAL+$MEMORY_TOTAL_THIS_GPU))
+		MEMORY_USED=$(($MEMORY_USED+$MEMORY_USED_THIS_GPU))
+		UTILIZATION_GPU=$(($UTILIZATION_GPU+$UTILIZATION_GPU_THIS_GPU))
+	done
+	echo $MEMORY_TOTAL
+	echo $MEMORY_USED
+	# echo $(($UTILIZATION_GPU / $NUM_GPU))
+	echo $(($MEMORY_USED*100/$MEMORY_TOTAL))
 elif checkcommand glxinfo; then
 	TOTALVRAM="`glxinfo | grep -A2 -i GL_NVX_gpu_memory_info | egrep -i "dedicated" | cut -f2- -d ':' | gawk '{print $1}'`"
 	AVAILVRAM="`glxinfo | grep -A4 -i GL_NVX_gpu_memory_info | egrep -i "available dedicated" | cut -f2- -d ':' | gawk '{print $1}'`"
